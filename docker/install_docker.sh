30cdca474d196a8d0513e9c7925da225a6c38aed
Reiki <56806850+KazamiHazaki@users.noreply.github.com>
2023-03-29 06:36:06 -0700

    Update install_docker.sh

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -5,7 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install docker-ce -y
-sudo usermod -aG docker ubuntu > /tmp/docker-log.txt
+sudo usermod -aG docker ${USER} > /tmp/docker-log.txt
 sudo systemctl status docker > /tmp/cek-group.txt
 sudo docker pull hello-worldclear
 sudo docker run hello-world
